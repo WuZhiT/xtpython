a7e135b21fdbdb2e635e19d7da8388958207b6ca
WuZhiT <t618718@outlook.com>
2020-06-02 20:46:53 +0800

    add 3ird

---
+++ mystuff.py
@@ -0,0 +1,5 @@
+def apple():
+    print("I am apples!")
+
+#this is just a variable
+tangerine = "Living reflection of a dream."
---
+++ xt40c.py
@@ -0,0 +1,11 @@
+class MyStuff(object):
+    def _init_(self):
+        self.tangerine = "And now a thousond years between"
+
+    def apple(self):
+        print("I AM CLASST APPLES!")
+
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
